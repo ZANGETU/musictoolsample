7f80f9ff4b489a3fc0f8a9eb165cd4e99e00fb4a
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-09-06 19:14:02 +0900

    Edited `Main.cs`

--- music sample/main.cs
+++ music sample/main.cs
@@ -1,54 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace music_sample
 {
     public partial class main : Form
     {
-        public main()
-        {
+        public main(){
             InitializeComponent();
         }
 
-        private void main_Load(object sender, EventArgs e)
-        {
-
+        private void main_Load(object sender, EventArgs e){
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Title = "音楽ファイルを選んでね";
-            openFileDialog1.InitialDirectory = @"C:/Desktop/";
-            openFileDialog1.FileName = "";
-            openFileDialog1.Filter = "wavファイル|*.wav|mp3ファイル|*.mp3";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true; 
-             openFileDialog1.Multiselect = true;
-
-            openFileDialog1.ReadOnlyChecked = true;
-            openFileDialog1.Dispose();
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        private void button1_Click(object sender, EventArgs e){
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "音楽ファイルを選んでね";
+            ofd.InitialDirectory = @"C:/Desktop/";
+            ofd.FileName = "";
+            ofd.Filter = "オーディオファイル | *.mp3; *.wav;";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true; 
+            /*
+            * ofd.Multiselect = true;
+            */
+            ofd.ReadOnlyChecked = true;
+            ofd.Dispose();
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                textBox1.Text = (openFileDialog1.FileName);
+                textBox1.Text = (ofd.FileName);
             }
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
+        private void button2_Click(object sender, EventArgs e){
             axWindowsMediaPlayer1.settings.autoStart = true;
             axWindowsMediaPlayer1.URL = (textBox1.Text);
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
+        private void button3_Click(object sender, EventArgs e){
             axWindowsMediaPlayer1.Ctlcontrols.stop();
         }
     }
